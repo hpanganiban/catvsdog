d3049c90365eed9a1807fe51728fffde66d77f53
hpanganiban <panganiban.hannah@gmail.com>
2018-10-29 22:37:28 +0800

    added sounds on hover

---
+++ assets/js/sound-mouseover.js
@@ -0,0 +1,83 @@
+
+function playclip1() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "../../audio-meow.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("meow"); audio.play();
+audio.play();
+}
+}
+}
+
+
+function playclip2() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "../../audio-bark.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("bark"); audio.play();
+audio.play();
+}
+}
+}
+
+
+function playclip3() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "../../audio-howl.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("howl"); audio.play();
+audio.play();
+}
+}
+}
+
+function playclip4() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "../../audio-pup.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("pup"); audio.play();
+audio.play();
+}
+}
+}
+
+
+function playclip5() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "../../audio-barkonce.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("barkonce"); audio.play();
+audio.play();
+}
+}
+}
